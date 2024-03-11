605749f1eea0b4d507d43aff343c92ff462541ce
Thomas Barenghi <90881270+thomasbarenghi@users.noreply.github.com>
2024-03-11 01:00:44 -0300

    feat: payments

--- client/src/app/api/notification/route.ts
+++ client/src/app/api/notification/route.ts
@@ -1,15 +1,15 @@
 import { PaymentStatusApiEnum } from '@/interfaces'
 import { mpAccessToken } from '@/utils/constants/env.const'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import MercadoPagoConfig, { Payment } from 'mercadopago'
+// import MercadoPagoConfig, { Payment } from 'mercadopago'
 import { cookies } from 'next/headers'
 import type { NextRequest } from 'next/server'
 
 export const POST = async (request: NextRequest) => {
   const cookieStore = cookies()
   const supabase = createServerComponentClient<Database>({ cookies: () => cookieStore })
-  const client = new MercadoPagoConfig({ accessToken: mpAccessToken })
-  const payment = new Payment(client)
+  // const client = new MercadoPagoConfig({ accessToken: mpAccessToken })
+  // const payment = new Payment(client)
   const searchParams = request.nextUrl.searchParams
   const topic = searchParams.get('topic') || searchParams.get('type')
   const orderId = searchParams.get('orderId')
@@ -19,24 +19,44 @@ export const POST = async (request: NextRequest) => {
   try {
     if (topic === 'payment') {
       const paymentId = searchParams.get('id') || searchParams.get('data.id')
-      const paymentRes = await payment.get({ id: paymentId ?? '' })
-      const paymentStatus = paymentRes.api_response.status
+      // const paymentRes = await payment.get({ id: paymentId ?? '' })
+      // const paymentStatus = paymentRes.api_response.status
 
-      await supabase
-        .from('orders')
-        .update({ payment_status: PaymentStatusApiEnum.Completed })
-        .eq('id', orderId)
-        .select()
+      const response = await fetch('https://api.mercadopago.com/v1/payments/' + paymentId, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + mpAccessToken
+        }
+      })
 
-      return new Response(JSON.stringify({ payment, paymentStatus }), {
+      if (!response.ok) {
+        throw new Error('Failed to fetch data')
+      }
+
+      const data = await response.json()
+
+      if (data.status === 'approved') {
+        await supabase
+          .from('orders')
+          .update({ payment_status: PaymentStatusApiEnum.Completed })
+          .eq('id', orderId)
+          .select()
+      } else {
+        await supabase
+          .from('orders')
+          .update({ payment_status: PaymentStatusApiEnum.Failure })
+          .eq('id', orderId)
+          .select()
+      }
+
+      return new Response(data, {
         status: 200,
         headers: {
           'Content-Type': 'application/json'
         }
       })
     } else {
-      await supabase.from('orders').update({ payment_status: PaymentStatusApiEnum.Failure }).eq('id', orderId).select()
-
       return new Response(JSON.stringify({ message: 'Invalid topic' }), {
         status: 400,
         headers: {
